249217426cfd7ee0e55f299ecdeae047da6be841
Vladimir Krivoshapov <v.kryvoshapov@gmail.com>
2015-07-23 10:44:07 +0300

    add Menu class

---
+++ src/nodes/Menu.cpp
@@ -0,0 +1,11 @@
+#include "Menu.hpp"
+
+Menu::Menu() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Menu::~Menu() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/nodes/Menu.hpp
@@ -0,0 +1,10 @@
+#ifndef SRC_NODES_MENU_HPP_
+#define SRC_NODES_MENU_HPP_
+
+class Menu {
+public:
+	Menu();
+	virtual ~Menu();
+};
+
+#endif /* SRC_NODES_MENU_HPP_ */
