82bb6ff7eb1942af8aaebec2038d9ad66131a1a6
Vladimir Krivoshapov <v.kryvoshapov@gmail.com>
2015-07-23 12:54:12 +0300

    refactor makefile

--- makefile
+++ makefile
@@ -7,4 +7,19 @@ main.o:
 	g++ -c -g src/main.cpp
 
 clean:
-	rm main.o menu.exe
+	rm main2.o menu
+	
+	
+	
+#CXXFLAGS =  -O2 -g -Wall -fmessage-length=0
+#OBJS =      main.cpp
+#LIBS =
+#TARGET =      main.exe
+#
+#$(TARGET):  $(OBJS)
+#    $(CXX) -o $(TARGET) $(OBJS) $(LIBS)
+#    
+#all:    $(TARGET)
+#
+#clean:
+#    rm -f $(OBJS) $(TARGET)
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <typeinfo> // string s = typeid(YourClass).name()
-#include "Node.hpp"
-#include "MenuManager.hpp"
+//#include "Node.hpp"
+//#include "MenuManager.hpp"
 
 using namespace std;
 
-MenuManager menu;
+//MenuManager menu;
 
 int main() {
-	menu.addMenu("root menu")
+//	menu.addMenu("root menu")
+//
+//	.addNode("node 1")
+//		.addChild("child 1.1")
+//		.addChild("child 1.2")
+//		.addChild("child 1.3")
+//	.done()
+//
+//	.addNode("node 2")
+//		.addChild("child 2.1")
+//
+//		.addNode("node 2.2")
+//			.addChild("child 2.2.1")
+//			.addChild("child 2.2.2")
+//			.addChild("child 2.2.3")
+//		.done()
+//
+//		.addChild("child 2.3")
+//
+//		.addNode("node 2.4")
+//			.addChild("child 2.4.1")
+//			.addChild("child 2.4.2")
+//			.addChild("child 2.4.3")
+//		.done()
+//
+//		.addChild("child 2.5")
+//	.done()
+//
+//	.addChild("child 3")
+//	.addChild("child 4");
+//
+//	cout << "sizeof(menu): " << sizeof( menu.getMenu() ) << endl;
+//	cout << "sizeof(children): " << sizeof( menu.getMenu().getChildren() ) << endl;
+//	cout << "menuNodeNum: "<< menu.getMenu().getChildrenNum() << endl;
+//	cout << "menuNodeNum: "<< menu.getMenu().getChildren()[1]->getChildrenNum() << endl;
 
-	.addNode("node 1")
-		.addChild("child 1.1")
-		.addChild("child 1.2")
-		.addChild("child 1.3")
-	.done()
+	cout << "Hi Man!" << endl;
 
-	.addNode("node 2")
-		.addChild("child 2.1")
-
-		.addNode("node 2.2")
-			.addChild("child 2.2.1")
-			.addChild("child 2.2.2")
-			.addChild("child 2.2.3")
-		.done()
-
-		.addChild("child 2.3")
-
-		.addNode("node 2.4")
-			.addChild("child 2.4.1")
-			.addChild("child 2.4.2")
-			.addChild("child 2.4.3")
-		.done()
-
-		.addChild("child 2.5")
-	.done()
-
-	.addChild("child 3")
-	.addChild("child 4");
-
-	cout << "sizeof(menu): " << sizeof( menu.getMenu() ) << endl;
-	cout << "sizeof(children): " << sizeof( menu.getMenu().getChildren() ) << endl;
-	cout << "menuNodeNum: "<< menu.getMenu().getChildrenNum() << endl;
-	cout << "menuNodeNum: "<< menu.getMenu().getChildren()[1]->getChildrenNum() << endl;
 	return 0;
 }
