db17bf318bf44574600b4ef814e54a616867aa91
vladimir <v.kryvoshapov@gmail.com>
2015-08-01 06:16:00 +0300

    Add 'Number' class. Add *.o to gitignore

---
+++ src/nodes/Number.cpp
@@ -0,0 +1,11 @@
+#include "Number.hpp"
+
+Number::Number() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Number::~Number() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/nodes/Number.hpp
@@ -0,0 +1,10 @@
+#ifndef SRC_NODES_NUMBER_HPP_
+#define SRC_NODES_NUMBER_HPP_
+
+class Number {
+public:
+	Number();
+	virtual ~Number();
+};
+
+#endif /* SRC_NODES_NUMBER_HPP_ */
