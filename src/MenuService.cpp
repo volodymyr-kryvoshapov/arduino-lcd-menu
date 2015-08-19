560c2a66aa01be81fffd584edcbafd0d204f8d6c
Vladimir Krivoshapov <v.kryvoshapov@gmail.com>
2015-08-19 17:57:15 +0300

    create MenuService class

---
+++ src/MenuService.cpp
@@ -0,0 +1,11 @@
+#include "MenuService.hpp"
+
+MenuService::MenuService() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MenuService::~MenuService() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/MenuService.hpp
@@ -0,0 +1,12 @@
+#ifndef SRC_MENUSERVICE_HPP_
+#define SRC_MENUSERVICE_HPP_
+
+class MenuService {
+
+
+public:
+	MenuService();
+	virtual ~MenuService();
+};
+
+#endif /* SRC_MENUSERVICE_HPP_ */
